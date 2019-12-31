6e2e5e5cc8c9a6464d65ea388b661a14bf857a22
seandai318 <seandai318@gmail.com>
2019-12-30 22:36:49 -0600

    remove obsolete files

--- tu/include/sipTU.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SIP_TU_H
-#define _SIP_TU_H
-
-#include "sipTransaction.h"
-
-osStatus_e sipTU_onMsg(sipTransMsgType_e msgType, void* pMsg);
-
-
-#endif
--- tu/include/sipTransport.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SIP_TRANSPORT_H
-#define _SIP_TRANSPORT_H
-
-#include "sipTransMgr.h"
-
-osStatus_e sipTransport_send(sipTransSipMsgType_e msgType, sipTransaction_t* pTrans);
-
-
-#endif
