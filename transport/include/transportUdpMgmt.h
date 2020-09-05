f8e4ab9d57e5433a79cc676868937956951e36ef
seandai318 <seandai318@gmail.com>
2020-09-05 16:11:09 -0500

    20200905, missed addinto repository

---
+++ transport/include/transportUdpMgmt.h
@@ -0,0 +1,15 @@
+/* Copyright 2020, Sean Dai
+ */
+
+#ifndef _TRANSPORT_UDP_MGMT_H
+#define _TRANSPORT_UDP_MGMT_H
+
+#include <netinet/in.h>
+
+#include "transportIntf.h"
+
+int tpUdpMgmtGetFd(transportAppType_e appType, struct sockaddr_in localAddr);
+tpLocalSendCallback_h tpUdpMgmtGetUdpCallback(int fd);
+osStatus_e tpUdpMgmtSetFd(transportAppType_e appType, struct sockaddr_in localAddr, tpLocalSendCallback_h callback, int fd);
+
+#endif
